e1e44d3a4476e0d742e95ca688b0075bacfd7a3b
RobertoFloris <florisroberto99@gmail.com>
2024-12-03 17:09:40 +0100

    [add] btn to load other 10 mail

--- script.js
+++ script.js
@@ -2,15 +2,29 @@ const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 const mail = document.getElementById("mail");
 const loading = document.getElementById("loading");
+const btn = document.getElementById("btn");
 
 let cont=0;
-
+loading.innerHTML = "Caricamento delle mail in corso..."
 mail.innerHTML="";
+btn.innerHTML="Clicca qui per caricare altre email"
 
 for(let i=0; i<10; i++){
   getApi()
 }
 
+
+btn.addEventListener("click", (e)=>{
+  mail.innerHTML="";
+  cont=0;
+  e.preventDefault();
+  mail.classList.add("d-none");
+  loading.classList.remove("d-none");
+  btn.classList.add("d-none")
+  for(let i=0; i<10; i++){
+    getApi()
+  }
+})
 
 
 function getApi(){
@@ -22,6 +36,7 @@ function getApi(){
   if(cont>9){
     mail.classList.remove("d-none");
     loading.classList.add("d-none");
+    btn.classList.remove("d-none");
   }
   })
   .catch(error => {
