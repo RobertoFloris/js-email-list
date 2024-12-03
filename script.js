9b75a7ed14b990a56b377c7a2df8fe1bfe6f08e3
RobertoFloris <florisroberto99@gmail.com>
2024-12-03 15:42:32 +0100

    [fix] innerHTML 10 mail

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
+let mail = document.getElementById("mail");
+mail.innerHTML="";
 
 for(let i=0; i<10; i++){
   getApi()
 }
 
 
+document.getElementById("mail")
+
+
 function getApi(){
   axios.get(endpoint)
   .then(response =>{
   console.log(response.data.response);
+  mail.innerHTML+=`<li>${response.data.response}</li>`
   })
   .catch(error => {
   console.log(error);
