bc66d68c09cd51fefdc771167146b2b8b38e4847
RobertoFloris <florisroberto99@gmail.com>
2024-12-03 15:27:31 +0100

    [fix] to console.log one mail

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 axios.get(endpoint)
 .then(response =>{
-  console.log(response.data);
+  console.log(response.data.response);
 })
 .catch(error => {
   console.log(error);
