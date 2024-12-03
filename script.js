465118e6b432143caa250303c57b69aacc4f472e
RobertoFloris <florisroberto99@gmail.com>
2024-12-03 16:53:32 +0100

    [add] logic to loading

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
-let mail = document.getElementById("mail");
+const mail = document.getElementById("mail");
+const loading = document.getElementById("loading");
+
+let cont=0;
+
 mail.innerHTML="";
 
 for(let i=0; i<10; i++){
@@ -8,14 +12,17 @@ for(let i=0; i<10; i++){
 }
 
 
-document.getElementById("mail")
-
 
 function getApi(){
   axios.get(endpoint)
   .then(response =>{
   console.log(response.data.response);
   mail.innerHTML+=`<li>${response.data.response}</li>`
+  cont++;
+  if(cont>9){
+    mail.classList.remove("d-none");
+    loading.classList.add("d-none");
+  }
   })
   .catch(error => {
   console.log(error);
